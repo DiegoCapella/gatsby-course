99665ec8d3d09b63ac714a8f5323d3c0499c95f4
DiegoCapella <diegocapelladev@gmail.com>
2021-01-19 15:06:10 -0300

    Create Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrepper">
+    <h1>Profile</h1>
+    <h2>Programador Banckend</h2>
+    <p>Lrem</p>
+  </div>
+)
+
+export default Profile
