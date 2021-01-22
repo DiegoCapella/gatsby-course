4ea13e4105b4d84586ba5b069b11bf8b8b032475
DiegoCapella <diegocapelladev@gmail.com>
2021-01-22 14:19:35 -0300

    Styled profile and sidebar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
     title: `My blog`,
-    position: `Frontend Develop`,
+    position: `Frontend Developer`,
     description: `A blog about frontend development and another cool stuff.`,
     author: `@myblog`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
