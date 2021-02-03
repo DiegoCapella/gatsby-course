6b26a72220876fc9539f4ebdee8a2b41ba017d07
DiegoCapella <diegocapelladev@gmail.com>
2021-02-03 14:41:36 -0300

    Add SEO

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import RecommendedPosts from "../components/RecommendedPosts"
 import Comments from "../components/Comments"
+
 import * as S from "../components/Post/styled"
 
-const BlogPost = ({data, pageContext}) => {
+const BlogPost = ({ data, pageContext }) => {
   const post = data.markdownRemark
   const next = pageContext.nextPost
   const previous = pageContext.previousPost
 
   return (
     <Layout>
-      <SEO title={post.frontmatter.title} />
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description}
+        
+      />
       <S.PostHeader>
         <S.PostDate>
-          {post.frontmatter.date} - {post.timeToRead} min de leitura
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
         </S.PostDate>
         <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
         <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
       </S.PostHeader>
       <S.MainContent>
-        <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPosts next={next} previous={previous} />
       <Comments url={post.fields.slug} title={post.frontmatter.title} />
@@ -32,19 +37,20 @@ const BlogPost = ({data, pageContext}) => {
 }
 
 export const query = graphql`
-  query Post($slug: String) { 
-    markdownRemark(fields: { slug: { eq: $slug } }) { 
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       fields {
         slug
       }
-      frontmatter { 
+      frontmatter {
         title
         description
-        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY" ) 
-      } 
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+        
+      }
       html
       timeToRead
-    } 
+    }
   }
 `
 
